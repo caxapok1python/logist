8d815f4b7acb557a00c46dd28f050f604942efd1
caxapok <caxapok.js@yandex.ru>
2022-12-13 20:22:37 +0300

    fix

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from robot import *
 
 if __name__ == '__main__':
     robot = Robot('/dev/ttyUSB0')
-    robot.setup_motors((50, 48, 6), (44, 42, 5), max_power=0.25, k=4)
+    robot.setup_motors((50, 48, 6), (44, 42, 5), max_power=0.25, k=3)
     robot.setup_camera(0)
 
-    robot.camera.set_line_params(dt=100, work_pos=150, work_width=500, work_height=20, blur=13)
+    robot.camera.set_line_params(dt=100, work_pos=120, work_width=450, work_height=20, blur=13)
     callback = Callback(robot)
     robot.camera.track_line_auto(callback.follow_line)
