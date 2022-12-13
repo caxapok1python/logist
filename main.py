86442a9f0ca13b4707e4567dd2329118926cdbb6
caxapok <caxapok.js@yandex.ru>
2022-12-13 20:02:25 +0300

    fix

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from robot import *
 
 if __name__ == '__main__':
     robot = Robot('/dev/ttyUSB0')
-    robot.setup_motors((50, 48, 6), (44, 42, 5), max_power=0.35, k=2.1)
+    robot.setup_motors((50, 48, 6), (44, 42, 5), max_power=0.3, k=4)
     robot.setup_camera(0)
 
     robot.camera.set_line_params(dt=100, work_pos=200, work_width=500, work_height=20, blur=13)
