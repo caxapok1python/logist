99faaf69247bdb458d8663f0a6502299160f74b4
caxapok <caxapok.js@yandex.ru>
2022-12-13 18:14:12 +0300

    fix

--- grab_image.py
+++ grab_image.py
@@ -2,23 +2,25 @@ import cv2
 import numpy as np
 
 
-def apply_filter(img):
+def apply_filter(img, dt):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (13, 13), 0)
-    filter = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 5, 29)
+    filter = cv2.threshold(gray, dt, 255, cv2.THRESH_BINARY_INV)
     filter = cv2.bitwise_not(filter, np.ones(filter.shape, filter.dtype))
     return filter
 
 
 cap = cv2.VideoCapture(0)
+r = [60, 70, 80, 90, 100, 130, 150]
 
 while True:
     try:
         ret, image = cap.read()
         cv2.imwrite('../tmp/tmp.png', image)
         print(image.shape)
-        th = apply_filter(image)
-        cv2.imwrite(f'../tmp/filter.png', th)
+        for i in r:
+            th = apply_filter(image, i)
+            cv2.imwrite(f'../tmp/{i}.png', th)
         cv2.waitKey(1)
 
     except KeyboardInterrupt:
