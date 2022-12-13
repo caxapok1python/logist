fdd27c0a59458e09afa7dfbea75d988a5c503199
caxapok <caxapok.js@yandex.ru>
2022-12-13 16:39:22 +0300

    fix

--- grab_image.py
+++ grab_image.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
 
-def apply_filter(dt, img):
+
+def apply_filter(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (13, 13), 0)
     filter = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 31, 15)
