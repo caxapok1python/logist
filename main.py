96076b883108d25232199fe860a3b6de191122ca
caxapok <caxapok.js@yandex.ru>
2022-12-13 17:26:27 +0300

    fix

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     robot.setup_motors((44, 42, 5), (50, 48, 6), max_power=0.5, k=7)
     robot.setup_camera(0)
 
-    robot.camera.set_line_params(dt=0, work_pos=400, work_width=245, work_height=20, blur=13)
+    robot.camera.set_line_params(dt=0, work_pos=300, work_width=400, work_height=20, blur=13)
     callback = Callback(robot)
     robot.camera.track_line_auto(callback.follow_line)
