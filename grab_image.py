a1c7d23563ee24af4195fb585994b73ae2c35ae6
caxapok <caxapok.js@yandex.ru>
2022-12-13 16:33:49 +0300

    grab

---
+++ grab_image.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+
+def apply_filter(dt, img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (13, 13), 0)
+    filter = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 31, 15)
+    filter = cv2.bitwise_not(filter, np.ones(filter.shape, filter.dtype))
+    return filter
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    try:
+
+        ret, image = cap.read()
+        cv2.imwrite('../tmp/tmp.png', image)
+        print(image.shape)
+        th = apply_filter(image)
+        cv2.imwrite(f'../tmp/filter.png', th)
+        cv2.waitKey(1)
+
+    except KeyboardInterrupt:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
