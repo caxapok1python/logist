235344c2d1df88c57f990069fe8ff6a2c9b36364
caxapok <caxapok.js@yandex.ru>
2022-12-14 19:30:16 +0300

    pid test

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import cv2
 import numpy as np
 from robot import *
+import argparse
+
+kp = 1.0
+ki = 0.0
+kd = 0.0
+dt = 1.0
 
 if __name__ == '__main__':
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--power", type=int, default=0.25, help="Maximum power")
+
     robot = Robot('/dev/ttyUSB0')
-    robot.setup_motors((50, 48, 6), (44, 42, 5), max_power=0.25, k=2)
+    robot.setup_motors((50, 48, 6), (44, 42, 5), max_power=0.25)
     robot.setup_camera(0)
 
     robot.camera.set_line_params(dt=100, work_pos=190, work_width=350, work_height=20, blur=13)
     callback = Callback(robot)
+    callback.configurate_pid(kp, ki, kd, dt)
     robot.camera.track_line_auto(callback.follow_line)
--- robot/__init__.py
+++ robot/__init__.py
@@ -26,9 +26,38 @@ class Robot:
         self.camera.stop()
 
 
+class PID:
+    def __init__(self, kp, ki, kd, dt=1.0):
+        self.dt = dt
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.err = 0
+        self.last_err = self.err
+        self.sum_err = self.err
+        self.derr = 0
+        self.setpoint = 0
+        self.min = -20
+        self.max = 20
+
+    def in_range(self, out):
+        return min(max(self.min, out), self.max)
+
+    def regulator(self, val):
+        self.err = self.setpoint - val
+        self.sum_err += self.err * self.dt
+        self.derr = (self.err - self.last_err) / self.dt
+
+        self.last_err = self.err
+
+        out = self.kp * self.err + self.ki * self.sum_err + self.kd * self.derr
+        return self.in_range(out)
+
+
 class Callback:
     def __init__(self, robot: Robot):
         self.robot = robot
+        self.pid = PID(0.0, 0.0, 0.0, 1.0)
 
     def calculate_angle(self, line_center):
         alc = (0 + int((self.robot.camera.width - self.robot.camera.work_width) / 2) + line_center[0],
@@ -38,9 +67,32 @@ class Callback:
         # dx, dy = (self.robot.camera.work_width // 2 - line_center[0], self.robot.camera.work_height)
         return float(np.degrees(atan2(dx, dy)))
 
+    def configurate_pid(self, kp=0.0, ki=0.0, kd=0.0, dt=1.0):
+        self.pid.setpoint = 0
+        self.pid.kp = kp
+        self.pid.ki = ki
+        self.pid.kd = kd
+        self.pid.dt = dt
+        self.pid.min = 0
+        self.pid.max = self.robot.chassis.statpower
+
     def follow_line(self, *args):
-        print(args)
-        print(args[0][0], self.robot.camera.work_width // 2)
         angle = self.calculate_angle(*args)
+        delta = self.pid.regulator(val=angle)
+
+        left = self.robot.chassis.statpower
+        right = self.robot.chassis.statpower
+        if angle == 0:
+            left = 0
+            right = 0
+        elif angle < 0:
+            right -= delta
+        else:
+            left -= delta
+
+        self.robot.chassis.set_power(left, right)
+
+        print(args)
         print(angle)
-        self.robot.chassis.direction(angle)
+        print(args[0][0], self.robot.camera.work_width // 2)
+        # self.robot.chassis.direction(angle)
