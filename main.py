3204eb1def101770fa1c56f99131e2c929fca34d
caxapok <caxapok.js@yandex.ru>
2022-12-14 20:58:24 +0300

    pid

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     # parser.add_argument("--power", type=int, default=0.25, help="Maximum power")
 
     robot = Robot('/dev/ttyUSB0')
-    robot.setup_motors((50, 48, 6), (44, 42, 5), max_power=0.25)
+    robot.setup_motors((44, 42, 5), (50, 48, 6), max_power=0.25)
     robot.setup_camera(0)
 
     robot.camera.set_line_params(dt=130, work_pos=30, work_width=500, work_height=20, blur=13)
