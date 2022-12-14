5f215f4614f79bde8be8f4d544beb8d4b60dbe40
caxapok <caxapok.js@yandex.ru>
2022-12-14 20:08:35 +0300

    pid

--- grab_image.py
+++ grab_image.py
@@ -10,6 +10,15 @@ def apply_filter(img):
     return filter
 
 
+def apply_dt(img, dt):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (13, 13), 0)
+    _, filter = cv2.threshold(gray, dt, 255, cv2.THRESH_BINARY_INV)
+    # filter = cv2.bitwise_not(filter, np.ones(filter.shape, filter.dtype))
+    return filter
+
+a = [100, 110, 130, 140, 150]
+
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -18,6 +27,9 @@ while True:
         cv2.imwrite('../tmp/tmp.png', image)
         print(image.shape)
         th = apply_filter(image)
+        for i in a:
+            img = apply_dt(image, i)
+            cv2.imwrite(f'../tmp/{i}.png', img)
         cv2.imwrite(f'../tmp/filter.png', th)
         cv2.waitKey(1)
 
